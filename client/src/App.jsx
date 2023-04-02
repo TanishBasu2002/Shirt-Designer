d5afee60120675d99e32f758480a9fc244766439
Tanish2002Basu <>
2023-04-03 01:12:19 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
 import Canvas from './canvas'
-import Customizer from './page/Customizer'
-import Home from './page/Home'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
 function App() {
   
 
   return (
     <div className="App">
-      <h1 className='head-text' >Shirt</h1>
+      <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
     </div>
   )
 }
